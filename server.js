9ab7bb904b892933c3632a186bafd320fb38aeb0
19215915 <39890965+19215915@users.noreply.github.com>
2018-06-04 14:03:54 -0500

    update

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/football/index.html'));
 });
 app.listen(process.env.PORT || 8080);
