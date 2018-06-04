c578e70460da6e57bdc4ced5329d7c34534e150a
19215915 <39890965+19215915@users.noreply.github.com>
2018-06-04 14:20:51 -0500

    changes

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
  
-app.use(express.static('build'));
+app.use(express.static(__dirname + '/dist/finalfootball'));
 app.get('/*', function(req,res) {    
-res.sendFile(path.join('index.html'));
+res.sendFile(path.join(__dirname+'/dist/finalfootball/index.html'));
 });
 app.listen(process.env.PORT || 8080);
