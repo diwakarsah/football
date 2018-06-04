f9f3e6063edb89d5fa8138e34a83fe7be065c46c
19215915 <39890965+19215915@users.noreply.github.com>
2018-06-04 14:12:50 -0500

    fixed

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
  
-app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/football/index.html'));
+app.use(express.static('build'));
+app.get('/*', function(req,res) {    
+res.sendFile(path.join('index.html'));
 });
 app.listen(process.env.PORT || 8080);
