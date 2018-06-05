b0e0efd9c972b91b8e0c54eda96645564babb0ef
19215915 <39890965+19215915@users.noreply.github.com>
2018-06-05 08:01:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'dist')));
 app.listen(process.env.PORT || 8080);
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   console.log("error------"+req);
   res.sendFile(path.join(__dirname + '/dist/football/index.html'));
 });
